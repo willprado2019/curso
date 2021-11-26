90b4e401fe2eb5170c02e44ccff293e382620450
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-11-26 17:40:41 -0300

    commit do fim da aula 7
    fim da aula 7

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from django.urls import path
 from .views import hello_world
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_world),
+  
 ]
