a6878ff2e9670f1af47c541dba9e41e614c70f44
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-11-26 18:03:49 -0300

    atualizou o models.
    Aula 8 aos 3:55, definiu os campos titulo e subtítulo além do conteúdo.

--- blog/website/models.py
+++ blog/website/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    sub_title = models.CharField(max_length=200)
+    content = models.TextField()
