74288d972c088c8ed069715b40bca560756738cb
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-11-27 16:27:29 -0300

    fim da aula 11

--- blog/website/views.py
+++ blog/website/views.py
@@ -6,5 +6,14 @@ from django.shortcuts import render
 #     return render(request, 'index.html')
 
 def hello_blog(request):
-    return render(request, 'indexgeral.html')
+    lista = [
+        'Django', 'Python', 'Git', 'HTML',
+        'Banco De Dados', 'Linux', 'Nginx', 'Uwsgi',
+        'Systemctl'
+    ]
+
+    data = {'name': 'Curso de Django 3',
+    'lista_tecnologias': lista
+    }
+    return render(request, 'index.html', data)
 
