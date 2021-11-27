a17eaa41970a701e8c991e9805baed52f71e2d67
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-11-27 15:27:29 -0300

    commit do meio da aula 10
    foram alterados os arqivos views.py, urls etc...

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import hello_world
 
 
@@ -8,5 +8,6 @@ from .views import hello_world
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_world),
+    path('blog/', include('website.urls')),
   
 ]
