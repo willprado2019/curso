8053f987419ee6887f0c01351add5cd9d907ccb3
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-11-27 15:31:15 -0300

    agora renderizou de index.html
    criou uma pasta template com o arquivo index.html e renderizou a palavra blog de uma tag h1.

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def hello_blog(request):
-    return HttpResponse('Blog')
+    return render(request, 'index.html')
 
