9445a9ff5151c4e251505060c79d535343a8c6f2
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-12-06 12:41:50 -0300

    alteração no admin Aula 14
    dentro da classe Post criou uma função chamada de  str que retorna o  titulo de um post lá no admin.

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -5,6 +5,8 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'sub_title']
     search_fields = ['title', 'sub_title']
+    fields = ('title', 'sub_title')
+
 
 
 admin.site.register(Post, PostAdmin)
--- blog/website/models.py
+++ blog/website/models.py
@@ -4,3 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+
+
+    def __str__(self):
+        return self.title
