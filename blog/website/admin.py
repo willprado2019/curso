b5277ea9712e8636f0a0a3c93c385bb83c65b37d
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-12-06 13:24:34 -0300

    fim da aula 15
    em models.py foi adicionado uma classe chamada de categorias e como argumento o objeto models com o método textChoices

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'sub_title', 'full_name']
+    list_display = ['title', 'sub_title', 'full_name', 'categories']
     search_fields = ['title', 'sub_title']
-    fields = ('title', 'sub_title')
+    # fields = ('title', 'sub_title')
 
 
 
