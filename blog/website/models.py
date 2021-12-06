22ba1e76fe48b63c54a7335b34a2247a5d7a9616
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-12-06 13:17:58 -0300

    Aula 15
    criou uma classe categorias em models.py

--- blog/website/models.py
+++ blog/website/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+
+class Categorias(models.TextChoices):
+    TECH = 'TC', 'Tecnologias'
+    CR = 'CR', 'Curiosidades' 
+    GR = 'GR', 'Geral'
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR
+    )
 
 
     def __str__(self):
