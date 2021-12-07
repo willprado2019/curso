9b97ac3e4490ea0427420d2bf1a208f2d8b29d16
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-12-07 11:18:42 -0300

    mudança no admin
    approved foi adicionado no list fields do arquivo admin

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'sub_title', 'full_name', 'categories']
+    list_display = ['title', 'sub_title', 'full_name',
+     'categories', 'approved']
     search_fields = ['title', 'sub_title']
     # fields = ('title', 'sub_title')
 
