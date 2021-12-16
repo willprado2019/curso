455c67bfbdf18d3cb8aa1b8b52c5d9b781afcf8e
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-12-16 19:18:19 -0300

    Alterações
    alterou o views, o urls e criou post_detail.html na pasta templates.

--- blog/website/urls.py
+++ blog/website/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from .views import hello_blog
-
-
+from .views import post_detail
 
 urlpatterns = [
     path('', hello_blog),
+    path('post/<int:id>/', post_detail, name='post_detail'),
 ]
