24f0896ad387fd9b5df8107ddd9d6ccb40650c15
willprado2019 <50236148+willprado2019@users.noreply.github.com>
2021-12-28 11:13:36 -0300

    Update models.py

--- blog/website/models.py
+++ blog/website/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     )
 
     deleted = models.BooleanField(default=True)
-    imagem = models.ImageField(upload_to='posts')
+    imagem = models.ImageField(upload_to='posts', null=True, blank=True)
 
     def __str__(self):
         return self.title
